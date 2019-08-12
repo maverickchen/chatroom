eeb1885ec042657985ca8ca1987333efca44c2be
maverickchen <mcmaverick001@gmail.com>
2019-08-11 22:56:00 -0400

    use pip3

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 echo "installing server dependencies..."
 cd server/
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 echo "installing react dependencies..."
 cd ../client-chat/
