3c8018d089eff65028ef869a98302b3d98680b9a
maverickchen <mcmaverick001@gmail.com>
2019-08-11 23:39:01 -0400

    run.sh exits on error, displays local address of server

--- run.sh
+++ run.sh
@@ -1,14 +1,18 @@
+set -e
 echo "installing server dependencies..."
 cd server/
 pip3 install --user -r requirements.txt
 
+echo
 echo "installing react dependencies..."
 cd ../client-chat/
 npm install
 
+echo 
 echo "building react app..."
 npm run build
 
-echo "setup finished, starting Flask server..."
+echo 
+echo "setup finished, starting Flask server at $(ipconfig getifaddr en0):5000..."
 cd ../server/
 python3 app.py
